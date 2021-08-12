fa8d494001c0ab9f471b9af775bac989786e6401
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-08-12 19:14:22 -0300

    codando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
+/* const welcome = {
+  greeting: 'Hey',
+  tittle: 'React'
+} */
+
+function getTitle(title) {
+  return title;
+}
+
 function App() {
+ /*  const tittle = "React" */
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello {getTitle('React')}</h1>
+       {/*  <h1>{welcome.greeting} {welcome.tittle}</h1> */}
       </header>
+      <label htmlFor="search">Search</label>
+      <input id="search" type="text"></input>
     </div>
   );
 }
