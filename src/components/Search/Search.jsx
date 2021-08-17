b0f58a1658e12090afb1549b472a8aa09a1fb95f
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-08-17 08:57:56 -0300

    componente search e list

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Search() {
+    return (
+        <>
+            <label className="exerc02" htmlFor="search">Search</label>
+            <input className="exerc02" id="search" type="text"></input>
+        </>
+    )
+}
+
+export default Search;
